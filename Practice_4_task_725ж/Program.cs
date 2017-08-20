41ac8d0114173a9762fe840d445301cad4f32d22
Maria <Maria@LAPTOP-N156UH30>
2017-08-20 19:49:45 +0500

    Решение, еще не протестированное

--- Practice_4_task_725ж/Program.cs
+++ Practice_4_task_725ж/Program.cs
@@ -1,30 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Practice_4_task_725ж
 {
     class Program
     {
+        static double f(double x)
+        //функция для подсчета функции x^4-4.1*x^3+x^2-5.1*x+4.1
+        {
+            return x * x * x * x - 4.1 * x * x * x + x * x - 5.1 * x + 4.1;
+        }
+
         static void Main(string[] args)
         {
             const double x = 4.1;//настоящий ответ для определения погрешности
-            Console.WriteLine("Решение уравнения x^4-4.1*x^3+x^2-5.1*x+4.1=0 на отрезке [3.7, 5] с заданной абсолютной погрешностью");
-            Console.WriteLine();
             double left = 3.7;//левый край границы отрезка
             double right = 5;//правый край границы отрезка
-            double xNew = left + (right - left) / 2;//искомое число
-            Double e; bool ok;// переменная для проверки
-            do
+            double xNew;//искомое число, полученное решение
+            Console.WriteLine("Решение уравнения x^4-4.1*x^3+x^2-5.1*x+4.1=0 на отрезке [3.7, 5] с заданной абсолютной погрешностью");
+            Console.WriteLine();
+
+            double e; bool ok;// переменная для проверки
+            do//начало проверки
             {
-                Console.Write("Введите абсолютную погрешность с точностью не более 16 знаков после запятой (Для получения более точного результата погрешность должна быть не более "+(Math.Abs(x-xNew))+"): ");
-                ok = Double.TryParse(Console.ReadLine(), out e);
+                Console.Write("Введите абсолютную погрешность с точностью не более 16 знаков после запятой (Для получения более точного результата погрешность должна быть не более 0,25): ");
+                ok = double.TryParse(Console.ReadLine(), out e);
                 if (!ok) Console.WriteLine("Неверный ввод!");
             } while (!ok);// конец проверки
 
-            //While ()
+            do//начинаем вычислять xNew
+            {
+                xNew = left + (right - left) / 2;//находим середину нового отрезка
+                if (f(xNew) > 0) right = xNew;//если полученное решение правее(больше) ответа, то полученное решение становится правым краем
+                else left = xNew;//если полученное прешение левее (меньше) ответа, то полученное решение становится левым краем               
+            } while (Math.Abs(x - xNew) > e);//пока разница между настоящим и полученным решением не станет меньше e
+
+            Console.WriteLine("Полученное решение: " + xNew);
+            Console.ReadLine();
         }
     }
 }
